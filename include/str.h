02aa31ea4c3d52169fc86e98ddf7f92ef17b4dd0
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-04-13 23:24:09 +0300

    update header for my string

--- include/str.h
+++ include/str.h
@@ -6,7 +6,6 @@
 
 typedef struct string string_t;
 
-string_t* str_create(const size_t length);
 void str_delete(string_t *str);
 
 string_t* str_from_c_str(const char *str);
@@ -18,10 +17,10 @@ void str_catc(string_t *dst, const char *src);
 void str_cat(string_t *dst, const string_t *src);
 
 void str_cpyc(string_t *dst, const char *src);
-extern void str_cpy(string_t *dst, const string_t *src);
+void str_cpy(string_t *dst, const string_t *src);
 
 uint8_t str_cmpc(const string_t *str_l, const char *str_r);
-extern uint8_t str_cmp(const string_t *str_l, const string_t *str_r);
+uint8_t str_cmp(const string_t *str_l, const string_t *str_r);
 
 extern size_t str_length(const string_t *str);
 
@@ -30,8 +29,5 @@ extern char* str_to_c_str(const string_t *str);
 size_t str_chr_pos(const string_t *str, const char chr);
 extern char* str_chr_ptr(const string_t *str, const char chr);
 void str_reverse(const string_t *str);
-
-extern int str_print(FILE *out_stream, const string_t *str);
-extern int str_println(FILE *out_stream, const string_t *str);
 
 #endif // _STRING_H_
