753276c1777d7634b55f5e7dcc51ad66d098e371
AlexCr4ckPentest <48525682+AlexCr4ckPentest@users.noreply.github.com>
2020-05-06 19:28:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -15,23 +15,19 @@
 void stack_test_case(void)
 {
     stack_t *stack = stack_create();
-    printf("stack_test_case(): stack created!, stack size: %ld\n",
-            stack_size(stack));
+    printf("stack_test_case(): stack created!, stack size: %ld\n", stack_size(stack));
 
     stack_push(stack, 100);
-    printf("stack_test_case(): pushing %d, stack size: %ld\n",
-            100, stack_size(stack));
+    printf("stack_test_case(): pushing %d, stack size: %ld\n", 100, stack_size(stack));
 
     stack_push(stack, 555);
-    printf("stack_test_case(): pushing %d, stack size: %ld\n",
-            500, stack_size(stack));
+    printf("stack_test_case(): pushing %d, stack size: %ld\n", 500, stack_size(stack));
     
 
     printf("stack_test_case(): stack top: %d\n", (int)stack_top(stack));
 
     stack_pop(stack);
-    printf("stack_test_case(): pop() called! stack size: %ld\n",
-            stack_size(stack));
+    printf("stack_test_case(): pop() called! stack size: %ld\n", stack_size(stack));
 
     printf("stack_test_case(): stack top: %d\n", (int)stack_top(stack));
 
@@ -41,47 +37,38 @@ void stack_test_case(void)
 void list_test_case(void)
 {
     list_t *list = list_create();
-    printf("list_test_case(): list created! list size: %ld\n",
-            list_size(list));
+    printf("list_test_case(): list created! list size: %ld\n", list_size(list));
 
     list_push_front(list, 100);
     printf("list_test_case(): pushing %d to front, list size: %ld\n",
             100, list_size(list));
 
     list_push_back(list, 200);
-    printf("list_test_case(): pushing %d to back, list size: %ld\n",
-            200, list_size(list));
+    printf("list_test_case(): pushing %d to back, list size: %ld\n", 200, list_size(list));
 
 
-    printf("list_test_case(): [0] = %d, [1] = %d\n",
-            (int)list_at(list, 0), (int)list_at(list, 1));
+    printf("list_test_case(): [0] = %d, [1] = %d\n", (int)list_at(list, 0), (int)list_at(list, 1));
 
-    printf("list_test_case(): first = %d, last = %d\n",
-            (int)list_first(list), (int)list_last(list));
+    printf("list_test_case(): first = %d, last = %d\n", (int)list_first(list), (int)list_last(list));
 
     list_pop_front(list);
-    printf("list_test_case(): pop front!, list size: %ld\n",
-            list_size(list));
+    printf("list_test_case(): pop front!, list size: %ld\n", list_size(list));
 
     list_push_back(list, 300);
-    printf("list_test_case(): pushing %d to back, list size: %ld\n",
-            300, list_size(list));
+    printf("list_test_case(): pushing %d to back, list size: %ld\n", 300, list_size(list));
 
     list_pop_back(list);
     printf("list_test_case(): pop back! list size: %ld\n", list_size(list));
 
     list_push_back(list, 400);
-    printf("list_test_case(): pushing %d to back, list size: %ld\n",
-            400, list_size(list));
+    printf("list_test_case(): pushing %d to back, list size: %ld\n", 400, list_size(list));
 
-    printf("list_test_case(): [0] = %d, [1] = %d\n",
-            (int)list_at(list, 0), (int)list_at(list, 1));
+    printf("list_test_case(): [0] = %d, [1] = %d\n", (int)list_at(list, 0), (int)list_at(list, 1));
 
     list_reverse(list);
     printf("list_test_case(): revesing list!\n");
 
-    printf("list_test_case(): [0] = %d, [1] = %d\n",
-            (int)list_at(list, 0), (int)list_at(list, 1));
+    printf("list_test_case(): [0] = %d, [1] = %d\n", (int)list_at(list, 0), (int)list_at(list, 1));
 
     list_delete(list);
 }
@@ -91,7 +78,7 @@ void string_test_case(void)
 
 int main(void)
 {
-    //stack_test_case();
+    stack_test_case();
     list_test_case();
     return 0;
 }
