93cbb705bdfd3fe880b45614136e8f88db3229ba
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-05-24 18:12:13 +0300

    update header for module algorithms (count.h)

--- include/algorithms/count.h
+++ include/algorithms/count.h
@@ -3,13 +3,13 @@
 #ifndef _COUNT_H_
 #define _COUNT_H_
 
-uint32_t single_count_if(int *arr_p, const size_t size, predicate pred);
-uint32_t single_count_if_not(int *arr_p, const size_t size, predicate pred);
+uint32_t single_count_if(int *restrict arr_p, const size_t size, predicate pred);
+uint32_t single_count_if_not(int *restrict arr_p, const size_t size, predicate pred);
 
-uint32_t double_count_if(int *arr_p, const size_t size, comparator_int comp);
-uint32_t double_count_if_not(int *arr_p, const size_t size, comparator_int comp);
+uint32_t double_count_if(int *restrict arr_p, const size_t size, comparator_int comp);
+uint32_t double_count_if_not(int *restrict arr_p, const size_t size, comparator_int comp);
 
-uint32_t summ_if(int *arr_p, const size_t size, predicate pred);
-uint32_t summ_all(int *arr_p, const size_t size);
+uint32_t summ_if(int *restrict arr_p, const size_t size, predicate pred);
+uint32_t summ_all(int *restrict arr_p, const size_t size);
 
 #endif // _COUNT_H_
