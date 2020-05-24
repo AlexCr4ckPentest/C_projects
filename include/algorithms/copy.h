0a1726d115f843dff2048606df9bcc18b804014d
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-05-24 18:12:23 +0300

    update header for module algorithms (copy.h)

--- include/algorithms/copy.h
+++ include/algorithms/copy.h
@@ -4,8 +4,12 @@
 #define _COPY_H_
 
 extern void copy(int *arr_dst, int *arr_src, const size_t size);
+extern void reverse_copy(int *arr_dst, int *arr_src, const size_t size, predicate pred);
 
 uint32_t copy_if(int *arr_dst, int *arr_src, const size_t size, predicate pred);
 uint32_t copy_if_not(int *arr_dst, int *arr_src, const size_t size, predicate pred);
+
+uint32_t reverse_copy_if(int *arr_dst, int *arr_src, const size_t size, predicate pred);
+uint32_t reverse_copy_if_not(int *arr_dst, int *arr_src, const size_t size, predicate pred);
 
 #endif // _COPY_H_
